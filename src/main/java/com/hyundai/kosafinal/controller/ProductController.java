a3d89631c48ee1fe015f3ac2c2f3fc00fdde1ec0
LEEJESEOK <junelle_js@naver.com>
2022-06-21 22:23:27 +0900

    KF-7  - 현재 카테고리, 전체 제품 수

--- src/main/java/com/hyundai/kosafinal/controller/ProductController.java
+++ src/main/java/com/hyundai/kosafinal/controller/ProductController.java
@@ -1,8 +1,14 @@
 package com.hyundai.kosafinal.controller;
 
+import com.hyundai.kosafinal.domain.CategoryDTO;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import sun.util.resources.cldr.teo.CalendarData_teo_KE;
 
 /**
  * @author LEE JESEOK
@@ -10,9 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProductController {
 
-    @GetMapping("/category")
-    public String category() {
+    @GetMapping("/category/{large}_{medium}_{small}")
+    public ModelAndView category(@PathVariable("large") String large, @PathVariable("medium") String medium,
+                         @PathVariable("small") String small, Model model) {
+        ModelAndView mav = new ModelAndView("category");
         System.out.println("category");
-        return "category";
+        CategoryDTO categoryDTO = new CategoryDTO(large, medium, small);
+        model.addAttribute("category", categoryDTO);
+
+        return mav;
     }
 }
--- src/main/java/com/hyundai/kosafinal/controller/ProductRestController.java
+++ src/main/java/com/hyundai/kosafinal/controller/ProductRestController.java
@@ -4,10 +4,7 @@ import com.hyundai.kosafinal.domain.*;
 import com.hyundai.kosafinal.entity.Criteria;
 import com.hyundai.kosafinal.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,19 +14,37 @@ import java.util.Map;
  * @author LEE JESEOK
  */
 @RestController
+@RequestMapping("/api/category")
 public class ProductRestController {
     @Autowired
     ProductService productService;
 
-    @GetMapping(value = "/category/{large}_{medium}_{small}_{pageNum}_{brand}")
+    @GetMapping(value = "/{large}_{medium}_{small}_{pageNum}_{brand}_{color}_{size}")
     public Map<String, Object> productList(@PathVariable("large") String large, @PathVariable("medium") String medium, @PathVariable("small") String small,
-                                           @PathVariable("pageNum") int pageNum, @PathVariable("brand") String brand) {
+                                           @PathVariable(value = "pageNum", required = false) Integer pageNum, @PathVariable("brand") String brand,
+                                           @PathVariable("color") String color, @PathVariable("size") String size,
+                                           @RequestParam(value = "minPrice", required = false) Integer minPrice, @RequestParam(value = "maxPrice", required = false) Integer maxPrice,
+                                           @RequestParam(value = "sortType", required = false) Integer sortType) {
+
+        System.out.println("pageNum : " + pageNum);
+
         String id = "";
-        String size = "";
-        String color = "";
-        int minPrice = 0, maxPrice = Integer.MAX_VALUE;
+
         CategoryDTO categoryDTO = new CategoryDTO(large, medium, small);
+        if (pageNum == null) {
+            pageNum = 1;
+        }
         Criteria criteria = new Criteria(pageNum, 12);
+        if (color == null)
+            color = "";
+        if (size == null)
+            size = "";
+        if (minPrice == null)
+            minPrice = 0;
+        if (maxPrice == null)
+            maxPrice = Integer.MAX_VALUE;
+        minPrice = Math.max(0, Math.min(minPrice, Integer.MAX_VALUE));
+        maxPrice = Math.min(Integer.MAX_VALUE, Math.max(maxPrice, 0));
 
         int count = productService.getFilteredProductCount(id, size, color, categoryDTO, brand, minPrice, maxPrice);
         PageDTO pageDTO = new PageDTO(criteria, count);
@@ -39,6 +54,7 @@ public class ProductRestController {
         List<SizeDTO> sizeList = productService.getFilteredSizeListWithPaging(id, size, color, categoryDTO, brand, minPrice, maxPrice, 0, criteria);
 
         Map<String, Object> map = new HashMap<>();
+        map.put("category", categoryDTO);
         map.put("count", count);
         map.put("page", pageDTO);
         map.put("productList", productList);
