99a58fa17aebfee928bdd747d6858c05e7a7bc30
youji <dbwlgna98@naver.com>
2022-07-03 14:40:59 +0900

    상품 리뷰 등록 기능 수정 및 추가

--- src/main/java/com/hyundai/kosafinal/config/SecurityConfig.java
+++ src/main/java/com/hyundai/kosafinal/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers("/member/login").permitAll() //login.html은 모든 사용자가 볼 수 있다.
+                .antMatchers("/product_review/insert").permitAll()
                 .antMatchers("/board/board").hasRole("USER")
                 .antMatchers("/mypage/modify").hasRole("USER")
                 .antMatchers("/mypage").hasRole("USER");
